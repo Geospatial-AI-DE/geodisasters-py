bf81f2aee6536547160a42928e31d28e5ff89e03
gisfromscratch <gisfromscratch@live.de>
2024-08-11 22:17:37 +0200

    Added the hotspots function

--- geodisasters/services.py
+++ geodisasters/services.py
@@ -18,12 +18,18 @@ def query(client: GeoRapidClient, from_date: date, to_date: date, format: OutFor
     :param to_date: The end value for the date range. Must not be greater than yesterday.
     :param format: Defines the output format.
 
-    :return: The most common locations as dictionary.
+    :return: The most common locations as geospatial point features using the specified output format.
     """
+
+    if None is from_date:
+        raise ValueError(f'You must specify the from_date!')
 
     if from_date < date(2023, 5, 24):
         raise ValueError(f'Invalid from_date! {from_date} is smaller than 2023-05-24.')
     
+    if None is to_date:
+        raise ValueError(f'You must specify the to_date!')
+
     today = datetime.now(timezone.utc).date()
     yesterday = today - timedelta(days=1)
     if yesterday < to_date:
@@ -33,6 +39,43 @@ def query(client: GeoRapidClient, from_date: date, to_date: date, format: OutFor
     params = {
         'from': from_date.isoformat(),
         'to': to_date.isoformat(),
+        'format': str(format)
+    }
+    response = requests.request('GET', endpoint, headers=client.auth_headers, params=params)
+    response.raise_for_status()
+
+    return response.json()
+
+def hotspots(client: GeoRapidClient, seen_date: date = None, format: OutFormat = OutFormat.GEOJSON):
+    """
+    Returns the hotspot locations related to natural disasters.
+
+    The date is optional. When not specified, we return the features from yesterday.
+    The underlying knowledge graph collects locations since 2023-05-24 and yesterday should be the latest available date.
+    The format can be geojson or esri.
+
+    :param client: The client instance to use for this query.
+    :param seen_date: The value for the date of interest. Must be in the range of [2023-05-24, yesterday].
+    :param format: Defines the output format.
+
+    :return: The hotspot locations as geospatial point features using the specified output format.
+    """
+
+    today = datetime.now(timezone.utc).date()
+    yesterday = today - timedelta(days=1)
+
+    if None is seen_date:
+        seen_date = yesterday
+
+    if seen_date < date(2023, 5, 24):
+        raise ValueError(f'Invalid seen_date! {seen_date} is smaller than 2023-05-24.')
+
+    if yesterday < seen_date:
+        raise ValueError(f'Invalid seen_date! {seen_date} is larger than yesterday.')
+    
+    endpoint = '{0}/hotspots'.format(client.url)
+    params = {
+        'date': seen_date.isoformat(),
         'format': str(format)
     }
     response = requests.request('GET', endpoint, headers=client.auth_headers, params=params)
--- tests/test_services.py
+++ tests/test_services.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifer: Apache-2.0
 
 from datetime import date
-from geodisasters.services import query
+from geodisasters.services import query, hotspots
 from georapid.factory import EnvironmentClientFactory
 from unittest import TestCase
 
@@ -15,6 +15,13 @@ class TestServices(TestCase):
     
     def test_query(self):
         geojson_result = query(self._client, date(2023, 5, 24), date(2023, 5, 24))
+        self.assertIsNotNone(geojson_result, "Result must not be None!")
+        self.assertTrue('features' in geojson_result, "GeoJSON response must have features!")
+        features = geojson_result['features']
+        self.assertTrue(isinstance(features, list), "GeoJSON features must be an instance of list!")
+
+    def test_hotspots(self):
+        geojson_result = hotspots(self._client, date(2023, 5, 24))
         self.assertIsNotNone(geojson_result, "Result must not be None!")
         self.assertTrue('features' in geojson_result, "GeoJSON response must have features!")
         features = geojson_result['features']
