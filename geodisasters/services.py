a11bcdeabe95f35011146712bec8372b9eb8ce8a
gisfromscratch <gisfromscratch@live.de>
2024-08-11 22:22:23 +0200

    Added the aggregate function

--- geodisasters/services.py
+++ geodisasters/services.py
@@ -82,3 +82,40 @@ def hotspots(client: GeoRapidClient, seen_date: date = None, format: OutFormat =
     response.raise_for_status()
 
     return response.json()
+
+def aggregate(client: GeoRapidClient, seen_date: date = None, format: OutFormat = OutFormat.GEOJSON):
+    """
+    Aggregates the broadcasted news related to natural disasters using a spatial grid and returns the features as hexagonal bins.
+
+    The date is optional. When not specified, we return the features from yesterday.
+    The underlying knowledge graph contains locations since 2023-05-24 and yesterday should be the latest available date.
+    The format can be geojson or esri.
+
+    :param client: The client instance to use for this query.
+    :param seen_date: The value for the date of interest. Must be in the range of [2023-05-24, yesterday].
+    :param format: Defines the output format.
+
+    :return: The aggregated geospatial hexagonal bins using the specified output format.
+    """
+
+    today = datetime.now(timezone.utc).date()
+    yesterday = today - timedelta(days=1)
+
+    if None is seen_date:
+        seen_date = yesterday
+
+    if seen_date < date(2023, 5, 24):
+        raise ValueError(f'Invalid seen_date! {seen_date} is smaller than 2023-05-24.')
+
+    if yesterday < seen_date:
+        raise ValueError(f'Invalid seen_date! {seen_date} is larger than yesterday.')
+    
+    endpoint = '{0}/aggregate'.format(client.url)
+    params = {
+        'date': seen_date.isoformat(),
+        'format': str(format)
+    }
+    response = requests.request('GET', endpoint, headers=client.auth_headers, params=params)
+    response.raise_for_status()
+
+    return response.json()
--- tests/test_services.py
+++ tests/test_services.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifer: Apache-2.0
 
 from datetime import date
-from geodisasters.services import query, hotspots
+from geodisasters.services import query, hotspots, aggregate
 from georapid.factory import EnvironmentClientFactory
 from unittest import TestCase
 
@@ -22,6 +22,13 @@ class TestServices(TestCase):
 
     def test_hotspots(self):
         geojson_result = hotspots(self._client, date(2023, 5, 24))
+        self.assertIsNotNone(geojson_result, "Result must not be None!")
+        self.assertTrue('features' in geojson_result, "GeoJSON response must have features!")
+        features = geojson_result['features']
+        self.assertTrue(isinstance(features, list), "GeoJSON features must be an instance of list!")
+
+    def test_aggregate(self):
+        geojson_result = aggregate(self._client, date(2023, 5, 24))
         self.assertIsNotNone(geojson_result, "Result must not be None!")
         self.assertTrue('features' in geojson_result, "GeoJSON response must have features!")
         features = geojson_result['features']
